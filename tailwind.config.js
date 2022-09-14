92255a0ffca5928fab67a8cd881ef2600129dd15
Samson Aderonmu <94833173+Superior212@users.noreply.github.com>
2022-09-14 17:07:08 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}","./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        flixiaBlue: "#13203c"
+      }
+    },
+  },
+  plugins: [],
+}
